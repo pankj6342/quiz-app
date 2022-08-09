8faf5a2e6252e72bbcdefab22b43f2228332172b
pankaj sharma <psbhardwaj.psb@gmail.com>
2022-08-09 23:59:56 +0530

    env variable addedI

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
-// const mongoURI =
-//   "mongodb+srv://admin:admin@collegeknot.gpmfw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const mongoURI =
+  process.env.MONGO_URI ||
   "mongodb://localhost:27017/quiz-app?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
-// "mongodb+srv://panda:panda@cluster0.4oeu3.mongodb.net/quizApp?retryWrites=true&w=majority";
 const connectToMongo = async () => {
   await mongoose
     .connect(mongoURI, { bufferCommands: false })
