677577587f8c0e9b353a4425de1a0392c717032c
pankaj sharma <psbhardwaj.psb@gmail.com>
2022-08-15 22:17:18 +0530

    api base address modified

--- customHooks/useTestPage.js
+++ customHooks/useTestPage.js
@@ -11,7 +11,7 @@ export const useTestPage = () => {
   const [testList, setTestList] = useState([]);
   const [currTestData, setCurrTestData] = useState(null);
   const [result, setResult] = useState();
-
+  const host = process.env.NODE_ENV !== "production" ? "${host}" : "";
   useEffect(() => {
     if (!testId) {
       const id = router.query?.testId;
@@ -28,7 +28,7 @@ export const useTestPage = () => {
     try {
       //pending: make creator dynamic:
       //pending: make host dynamic
-      const resp = await axios.post(`http://localhost:5000/api/test/create`, {
+      const resp = await axios.post(`${host}/api/test/create`, {
         title: data?.title,
         description: data?.description,
         creator: {
@@ -51,9 +51,7 @@ export const useTestPage = () => {
     try {
       const query = "";
       if (testid) query = `?testId=${testid}`;
-      const resp = await axios.get(
-        `http://localhost:5000/api/test/get/${query}`
-      );
+      const resp = await axios.get(`${host}/api/test/get/${query}`);
       setTestList(resp?.data);
     } catch (e) {
       console.log({ fetchTestError: e.message });
@@ -69,7 +67,7 @@ export const useTestPage = () => {
         testId = router.query?.testId;
       }
       const resp = await axios.post(
-        `http://localhost:5000/api/test/update/?testId=${testId}`,
+        `${host}/api/test/update/?testId=${testId}`,
         data
       );
       console.log("updated ");
@@ -87,9 +85,7 @@ export const useTestPage = () => {
         // console.log("data", currTestData);
         if (questions) setQuestionArray(questions);
       } else {
-        const testData = await axios.get(
-          `http://localhost:5000/api/test/get/${query}`
-        );
+        const testData = await axios.get(`${host}/api/test/get/${query}`);
         if (testid) setCurrTestData(testData?.data[0]);
         console.log("fetched", testData?.data[0]);
         const questions = testData?.data[0]?.questions;
@@ -111,7 +107,7 @@ export const useTestPage = () => {
     try {
       if (!testid) testid = testId;
       const resp = await axios.post(
-        `http://localhost:5000/api/test/submit/?testId=${testid}`,
+        `${host}/api/test/submit/?testId=${testid}`,
         {
           answerList,
         }
